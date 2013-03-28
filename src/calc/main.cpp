4acd216ff4167a18ec30725a3465b46d9440872f
NightM <zzhhgo@gmail.com>
2013-03-28 22:42:54 +0800

    add operator_priority  function

--- src/calc/main.cpp
+++ src/calc/main.cpp
@@ -43,6 +43,19 @@ string omit_spaces(string infix)
     return result;
 }
 
+int compare_priority(char a, char b)
+{
+    map<char,int>operator_priority;
+    operator_priority.insert(make_pair('+',1));
+    operator_priority.insert(make_pair('-',1));
+    operator_priority.insert(make_pair('*',2));
+    operator_priority.insert(make_pair('/',2));
+    operator_priority.insert(make_pair('%',2));
+    operator_priority.insert(make_pair('^',3));
+
+    return operator_priority[a]-operator_priority[b];
+}
+
 string check_string(string infix)
 {
     string result=infix;
@@ -128,7 +141,80 @@ string preprocess(string infix)
 }
 
 
+queue<string> infix_to_post(string infix) {
+    queue<string> postfix;
+    stack<char> operator_stack;
 
+    set<char> valid_operand_set;
+    for(int i=0; i<=9; i++) {
+        valid_operand_set.insert(i+'0');
+    }
+    valid_operand_set.insert('.');
+    valid_operand_set.insert('e');
+    valid_operand_set.insert('E');
+
+    for(int i=0; i<infix.size(); i++) {
+        cout<<endl;
+        char ch = infix[i];
+        cout<<"No:"<<"  "<<i<<"  char:  "<<ch<<endl;
+        if(infix[i]=='(') {
+            operator_stack.push(infix[i]);
+        } else if(infix[i]==')') {
+            while(!operator_stack.empty() && operator_stack.top()!='(') {
+                postfix.push(string(1, operator_stack.top()));
+                operator_stack.pop();
+            }
+            operator_stack.pop();
+        } else if(is_operator(infix[i]) == true) {
+            if(operator_stack.empty()) {
+                operator_stack.push(infix[i]);
+                continue;
+            }
+
+            char top_stack = operator_stack.top();
+            while(compare_priority(infix[i], top_stack)<=0) {
+                 if(compare_priority(infix[i], top_stack)==0 && infix[i]=='^') {   //因为'^'的结合性从右向左，所以单独处理
+                    break;
+                }
+                postfix.push(string(1, top_stack));
+                operator_stack.pop();
+                if(!operator_stack.empty()) {
+                    top_stack = operator_stack.top();
+                } else {
+                    break;
+                }
+
+            }
+            operator_stack.push(infix[i]);
+        } else {
+            string current_operator;
+            int j=i;
+            while(valid_operand_set.find(infix[j]) != valid_operand_set.end()) {
+                current_operator += infix[j];
+                ++j;
+            }
+            postfix.push(current_operator);
+            i=j-1;
+        }
+
+        cout<<"top_stack: "<<(operator_stack.empty() ?' ': operator_stack.top())<<endl;
+        queue<string> temp_queue = postfix;
+        cout<<"posfix: ";
+        while(temp_queue.size()>0) {
+            cout<<temp_queue.front()<<"  ";
+            temp_queue.front();
+            temp_queue.pop();
+        }
+        cout<<endl;
+
+    }
+    while(!operator_stack.empty()) {
+        postfix.push(string(1, operator_stack.top()));
+        operator_stack.pop();
+    }
+
+    return postfix;
+}
 
 double calculate_two(double a, double b,string operand)
 {
